2ea9ecb281dab7adf86d24d099ef269bb894f41c
A. Yasin Akalin <ayasinakalin@gmail.com>
2019-04-03 02:30:33 +0300

    Update utility.py
    Add function getConsent() to get binary input from the user.

--- src/utility.py
+++ src/utility.py
@@ -13,3 +13,17 @@ def openNotebook(verbose=True):
     response = sp.run(
                 "sh " + str(definitions.DIR_SCRIPT / "openJupyter.sh"),
                 shell=True)
+
+
+def getConsent(message='', trial=1):
+    passes = {'Y': True, 'N': False}
+    reply_original = input(message + "\t[Y/N]: ")
+    reply = reply_original.capitalize().strip()
+
+    if reply in passes.keys():
+        return passes[reply]
+    elif trial < 3:
+        print("Unrecognized reply:", reply_original)
+        return getConsent(message, trial=trial+1)
+    else:
+        return False
