1a1b5afc260209954906c6a5492571f2e70f6dcd
A. Yasin Akalin <ayasinakalin@gmail.com>
2019-04-06 22:00:17 +0300

    Update __main__.py
    New functions checks whether dependencies are satisfied or not; if so starts jupyter notebook, else install them.

--- src/__main__.py
+++ src/__main__.py
@@ -6,6 +6,13 @@ import utility
 import subprocess as sp
 print("Hello world!")
 definitions.init()
+
+# Check if dependencies are satisfied
+deps = utility.getDependencies(definitions.FILE_REQS)
+if utility.checkDependencies(deps, verbose=True):
+    utility.installRequirements()
+
+# Run Jupyter Notebook server (on local machine)
 if not utility.openNotebook():
     msg = "Do you want to install required packages?"
     if utility.getConsent(msg):
